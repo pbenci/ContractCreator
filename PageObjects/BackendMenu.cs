db97fae41ec07b9b2e9a8e344a0794fe638dc7c9
pbenci <53452060+pbenci@users.noreply.github.com>
2026-04-29 15:02:43 +0200

    Refactor BackendMenu to use By locators

--- PageObjects/BackendMenu.cs
+++ PageObjects/BackendMenu.cs
@@ -1,4 +1,4 @@
-﻿using OpenQA.Selenium;
+using OpenQA.Selenium;
 using System;
 
 namespace ContractCreator
@@ -8,16 +8,10 @@ namespace ContractCreator
         public IWebDriver Driver { get; set; }
         public Interactions Interaction { get; set; }
         public Waits Wait { get; set; }
-        public WebElements RentalMainMenuButton => new(Driver, By.Id("voceMenu_rentals"));
-        public WebElements BookingMenuButton => new(Driver, By.Id("voceMenu_booking"));        
-        public WebElements FleetMainMenuButton => new(Driver, By.Id("voceMenu_fleet"));
-        public WebElements SparePartsMenuButton => new(Driver, By.Id("voceMenu_spare_part"));
-        public WebElements Overlay => new(Driver, By.CssSelector("div > svg"), 2);
-        public WebElements CloseAllTabsButton => new(Driver, By.Id("close-all-tabs"));
-        public WebElements ConfirmClosingAllTabsButton => new(Driver, By.CssSelector("div.bootbox.modal.fade.bootbox-confirm.in > div > div > div.modal-footer > button.btn.btn-primary"));
-        public string ActiveTabId => Driver.FindElement(By.Id("tab-header")).FindElement(By.ClassName("active")).GetAttribute("id");
-        public WebElements CrudPageNumberButton => new(Driver, By.LinkText(string.Format($"{CrudNextPage}")));
-        protected int CrudNextPage { get; set; }
+        public By RentalMainMenuButton => By.Id("voceMenu_rentals");
+        public By BookingMenuButton => By.Id("voceMenu_booking");
+        public By Overlay => By.CssSelector("div > svg");
+        public By CloseAllTabsButton => By.Id("close-all-tabs");
 
         public BackendMenu(IWebDriver Driver)
         {
@@ -28,38 +22,9 @@ namespace ContractCreator
 
         public void WaitForOverlayToDisappear()
         {
-            try
-            {
-                Console.WriteLine("Yeah");
-                Wait.ForElementToExist(Overlay);
+            try {
                 Wait.ForElementToBeInvisible(Overlay);
-            }
-            catch (NoSuchElementException)
-            {
-                Console.WriteLine("Nope");
-            }
-        }
-
-        //public void WaitForOverlayToDisappear()
-        //{
-        //    if (Overlay.Elements.Count > 0)
-        //    {
-        //        Console.WriteLine("Yeah");
-        //        Wait.ForElementToBeInvisible(Overlay);
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("Nope");
-        //    }
-        //}
-
-        public void CloseAllTabs()
-        {
-            if (CloseAllTabsButton.Element.Displayed)
-            {
-                Interaction.Click(CloseAllTabsButton.Element);
-                Interaction.Click(ConfirmClosingAllTabsButton.Element);
-            }
+            } catch (WebDriverTimeoutException)
         }
     }
 }
