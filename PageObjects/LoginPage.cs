f505cad943d72eec04c0be8ec829ac45dac5c5f6
pbenci <53452060+pbenci@users.noreply.github.com>
2026-04-29 15:03:00 +0200

    Refactor LoginPage to use By locators directly

--- PageObjects/LoginPage.cs
+++ PageObjects/LoginPage.cs
@@ -1,18 +1,14 @@
-﻿using OpenQA.Selenium;
+using OpenQA.Selenium;
 
 namespace ContractCreator
 {
     public class LoginPage : BackendMenu
     {
-        private WebElements LoginEmailField => new(Driver, By.Id("signin_username"));
-        private WebElements LoginPasswordField => new(Driver, By.Id("signin_password"));
-        private WebElements LoginButton => new(Driver, By.ClassName("btn-primary"));
+        private By LoginEmailField => By.Id("signin_username");
+        private By LoginPasswordField => By.Id("signin_password");
+        private By LoginButton => By.ClassName("btn-primary");
 
-        public LoginPage(IWebDriver Driver) : base(Driver)
-        {
-            this.Driver = Driver;
-            Interaction = new Interactions(Driver);
-        }
+        public LoginPage(IWebDriver Driver) : base(Driver) { }
 
         public void GoToUrl()
         {
@@ -21,9 +17,9 @@ namespace ContractCreator
 
         public void Login()
         {
-            Interaction.Write(LoginEmailField.Element, Config.BackendUsername);
-            Interaction.Write(LoginPasswordField.Element, Config.BackendPassword);
-            Interaction.Click(LoginButton.Element);
+            Interaction.Write(LoginEmailField, Config.BackendUsername);
+            Interaction.Write(LoginPasswordField, Config.BackendPassword);
+            Interaction.Click(LoginButton);
             WaitForOverlayToDisappear();
         }
     }
